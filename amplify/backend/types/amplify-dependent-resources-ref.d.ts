5c52a6a7ce64adbc5b79c5da38061856b341d63f
saymon_shoab <mdsaymonshoab@gmail.com>
2022-03-24 17:39:41 +0600

    start amplify crud

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifyshoab": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
